f34793d717ab8cf95f0c06410a2d15dd0a5422e5
Paul Bennett <pbennett.uk@outlook.com>
2020-06-19 16:52:12 +0100

    JS tweaks

--- demos/js.js
+++ demos/js.js
@@ -5,6 +5,11 @@ function render() {
 	btn.innerText = `Count: ${count}`;
 }
 
+let random = () => {
+	return Math.floor(Math.random() * (40 - 10 + 1))
+}
+console.log(random())
+
 btn.addEventListener('click', () => {
 	// count from 1 to 10
 	if (count < 10) {
